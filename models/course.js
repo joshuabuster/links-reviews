5e556960b8549ee630cdbe1b2499b5c685c96756
joshuabuster <63068851+joshuabuster@users.noreply.github.com>
2020-08-31 08:53:09 -0700

    adds review schema and embeds it to the course schema

--- models/course.js
+++ models/course.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const reviewShema = new Schema({
+    content: String
+})
+
 const courseSchema = new Schema({
     name: String,
     locations: String,
     courseId: Schema.Types.ObjectId,
+    reviews: [reviewShema]
 });
 
 module.exports = mongoose.model('Course', courseSchema);
