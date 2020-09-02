d2c56e15dc454a8310557608059c4901890f7b65
joshuabuster <63068851+joshuabuster@users.noreply.github.com>
2020-09-01 21:33:03 -0700

    refactors delete function

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -40,13 +40,32 @@ function update(req, res) {
     })
 };
 
+// =============== NOT DELETING THE CORRECT REVEIEW BECAUSE THE IDX IS INCORRECT =================
+// =============== req.params.reviewId CANNOT BE FOUND IN THE ARRAY ==============================
+
 function delReview(req, res) {
     Course.findById(req.params.courseId, function(err, course) {
         let idx = course.reviews.indexOf(req.params.reviewId);
-        let newReview = course.reviews.splice(idx, 1);
-        let review = newReview[0];
+        console.log('course: ', course);
+        console.log('course.reviews: ', course.reviews);
+        console.log('reviewID: ', req.params.reviewId);
+        let newReviewArr = course.reviews.splice(idx, 1);
+        console.log('newReviewArr: ', newReviewArr);
+        let review = newReviewArr[0];
         course.save(function(err) {
             res.render('courses/show', { title: `${course.name}`, city: `${course.city}`, course, review})  
         })
     })
 };
+
+// ==================== NOT WORKING!! DONT KNOW WHY ==================================
+
+// function delReview(req, res) {
+//     Course.findById(req.params.courseId, function(err, course) {
+//         let review = course.reviews.filter(el => el._id !== req.params.reviewId)
+//         console.log('review: ', review);
+//         course.save(function(err) {
+//             res.render('courses/show', { title: `${course.name}`, city: `${course.city}`, course, review}) 
+//         })
+//     })
+// }
