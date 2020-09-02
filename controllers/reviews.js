000284aed8d2d0684d7f74df2edc64f7ba399fb2
joshuabuster <63068851+joshuabuster@users.noreply.github.com>
2020-09-01 21:52:58 -0700

    refactors delete function

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -43,20 +43,20 @@ function update(req, res) {
 // =============== NOT DELETING THE CORRECT REVEIEW BECAUSE THE IDX IS INCORRECT =================
 // =============== req.params.reviewId CANNOT BE FOUND IN THE ARRAY ==============================
 
-function delReview(req, res) {
-    Course.findById(req.params.courseId, function(err, course) {
-        let idx = course.reviews.indexOf(req.params.reviewId);
-        console.log('course: ', course);
-        console.log('course.reviews: ', course.reviews);
-        console.log('reviewID: ', req.params.reviewId);
-        let newReviewArr = course.reviews.splice(idx, 1);
-        console.log('newReviewArr: ', newReviewArr);
-        let review = newReviewArr[0];
-        course.save(function(err) {
-            res.render('courses/show', { title: `${course.name}`, city: `${course.city}`, course, review})  
-        })
-    })
-};
+// function delReview(req, res) {
+//     Course.findById(req.params.courseId, function(err, course) {
+//         let idx = course.reviews.indexOf(req.params.reviewId);
+//         console.log('course: ', course);
+//         console.log('course.reviews: ', course.reviews);
+//         console.log('reviewID: ', req.params.reviewId);
+//         let newReviewArr = course.reviews.splice(idx, 1);
+//         console.log('newReviewArr: ', newReviewArr);
+//         let review = newReviewArr[0];
+//         course.save(function(err) {
+//             res.render('courses/show', { title: `${course.name}`, city: `${course.city}`, course, review})  
+//         })
+//     })
+// };
 
 // ==================== NOT WORKING!! DONT KNOW WHY ==================================
 
@@ -69,3 +69,17 @@ function delReview(req, res) {
 //         })
 //     })
 // }
+
+// ============================ ANOTHER TRY ====================================
+// ========================= FindByIdAndDelete =================================
+
+function delReview(req, res) {
+    Course.findById(req.params.courseId, function(err, course) {
+        
+        course.reviews.findByIdAndDelete(req.params.reviewId, function(err, review) {
+            course.save(function(err) {
+                res.render('courses/show', { title: `${course.name}`, city: `${course.city}`, course, review}) 
+            })
+        })
+    })
+}
