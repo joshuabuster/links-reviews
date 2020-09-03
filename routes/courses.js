88352b1f286b1cfe7619cef43d3556e4736fef55
joshuabuster <63068851+joshuabuster@users.noreply.github.com>
2020-09-03 16:52:22 -0700

    deletes dead code

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const coursesCtrl = require('../controllers/courses');
-const reviewsCtrl = require('../controllers/reviews');
-
 
 
 router.get('/', coursesCtrl.index);
